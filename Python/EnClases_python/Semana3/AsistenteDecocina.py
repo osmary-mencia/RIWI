9509046cc945f3b8d1be919ac5b025537796ec91
Osmary Alejandra Mencia Gonzalez <osmarymencias@gmail.com>
2025-05-07 16:46:24 -0500

    Se agrego nuevo archivo de semana 3 taller asistente de cocina

---
+++ Python/EnClases_python/Semana3/AsistenteDecocina.py
@@ -0,0 +1,18 @@
+print('=================================')
+print('|| ASISTENTE VIRTUAL DE COCINA ||')
+print('=================================\n')
+
+try:
+    print('MENU DE OPCIONES: \n')
+    print('1. Ensalada César con pollo')
+    print('2. Wrap de pollo con salsa César')
+    print('3. Sándwich clásico de pollo\n')
+    
+    opcion_menu = int(input('Cual opcion desea escoger: '))
+    
+except ValueError:
+    print('Ingresa una opcion numerica, por favor:)')
+    
+
+    
+
