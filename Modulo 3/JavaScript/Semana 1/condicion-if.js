36c5ed6e9be3c344a2a4e16d64fef34f7e96f311
Osmary Alejandra Mencia Gonzalez <osmarymencias@gmail.com>
2025-06-18 16:16:01 -0500

    nuevos cambios

---
+++ Modulo 3/JavaScript/Semana 1/condicion-if.js
@@ -0,0 +1,9 @@
+const edad = 17 
+const tieneCarnet = true
+const puedeConducir = edad >= 18 && tieneCarnet
+
+if (puedeConducir){
+    console.log('Si puedes conducir')
+}else{
+    console.log('No puedes conducir')
+}
